b9309d34b426cb48e770e99074ec25469008b7eb
FosterTa <xxhedgemonxx@knights.ucf.edu>
2016-01-05 10:52:17 -0500

    Init commit

---
+++ mobile/src/main/java/comfostertaununcomsof/github/urbanwarfare/Main.java
@@ -0,0 +1,13 @@
+package comfostertaununcomsof.github.urbanwarfare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
